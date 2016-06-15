4d99398b3d1686a66d6c54c3a505308830851239
Danial Pervaiz <codemythought@gmail.com>
2016-06-15 14:49:45 -0600

    Added campus-trade-in

---
+++ campus-trade-in/js/custom.js
@@ -0,0 +1,48 @@
+
+// custom js files 
+
+/*
+	
+	1. Customized navigation effect on scrolling
+	2. backstrech image flipper header section. see documentation ( http://srobbin.com/jquery-plugins/backstretch/ )
+    3. activator on sign up or login form. when clicks it will activate the current button 
+
+*/
+
+/* 1. navigation scroll effect */
+
+    var navigation = $('.header').offset().top + 10;
+    $(document).scroll(function(){
+        if($(this).scrollTop() > navigation){
+            // $('.navbar-default').css({
+            //     'background-color':'rgba(43,51,65,0.5)',
+            //     'color':'#fff'
+            // });
+            // $('.navbar-default .navbar-brand').css({
+            //     'color':'#fff'
+            // });
+            // $('.navbar-right li a').css({
+            //     'color':'#fff',
+            // });
+            // $('.navbar-right li').css({
+            //     'border':'none'
+            // });
+        }
+    });
+
+/* 2. backstretch image flipper */
+
+     $(".header").backstretch([
+      "assets/header/1.jpg",
+      "assets/header/2.jpg",
+      "assets/header/3.jpg"
+      ], {
+        fade: 750,
+        duration: 4000
+      }); 
+    
+/* 3. activator */
+
+    $('#activator a').on('click', function(){
+        $(this).addClass('itemActive').siblings().removeClass('itemActive');
+    });
