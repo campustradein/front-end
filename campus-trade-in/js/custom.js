85896fa1b374c2d23b243b6a475e8cbf6c6ca2ea
Danial Pervaiz <codemythought@gmail.com>
2016-06-16 14:25:53 -0600

    Added Changes

--- campus-trade-in/js/custom.js
+++ campus-trade-in/js/custom.js
@@ -61,14 +61,6 @@
      
         // Most important owl features
         items : 4,
-        itemsCustom : false,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [980,3],
-        itemsTablet: [768,2],
-        itemsTabletSmall: false,
-        itemsMobile : [479,1],
-        singleItem : false,
-        itemsScaleUp : false,
 
         //Autoplay
         autoPlay : true,
